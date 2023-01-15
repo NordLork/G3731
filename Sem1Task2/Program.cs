6dfb94cdd418e3c7256c57edb7b56785fae7ce0b
NordLorK <nordlork@rambler.ru>
2023-01-15 18:27:26 +0300

    add solution for task 2,4,6,8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, 
+// которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// Считываем данные с консоли 1 число
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли 2 число
+string? inputNum2 = Console.ReadLine();
+// Проверим, чтобы данные были не пустыми
+if (inputNum1 != null && inputNum2 != null)
+{
+  // Парсим введённое 1 число
+  int number1 = int.Parse(inputNum1);
+  // Парсим введённое 2 число
+  int number2 = int.Parse(inputNum2);
+  // Срвниваем числа
+  if (number1 > number2)
+  {
+    Console.WriteLine("Первое число больше второго!");
+  }
+  else
+  {
+    Console.WriteLine("Второе число больше первого!");
+  }
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 4: Напишите программу,
+// которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+// Считываем данные с консоли 1 число
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли 2 число
+string? inputNum2 = Console.ReadLine();
+// Считываем данные с консоли 3 число
+string? inputNum3 = Console.ReadLine();
+// Проверим, чтобы данные были не пустыми
+if (inputNum1 != null && inputNum2 != null && inputNum3 != null)
+{
+  // Парсим введённое 1 число
+  int number1 = int.Parse(inputNum1);
+  // Парсим введённое 2 число
+  int number2 = int.Parse(inputNum2);
+  // Парсим введённое 3 число
+  int number3 = int.Parse(inputNum3);
+  // Сравниваем числа
+  if (number1 > number2)
+  {
+    if (number1 > number3)
+    {
+      Console.Write("Maximum: ");
+      Console.WriteLine(number1);
+    }
+    else
+    {
+      Console.Write("Maximum: ");
+      Console.WriteLine(number3);
+    }
+  }
+  else
+  {
+    if (number2 > number3)
+    {
+      Console.Write("Maximum: ");
+      Console.WriteLine(number2);
+    }
+    else
+    {
+      Console.Write("Maximum: ");
+      Console.WriteLine(number3);
+    }
+  }
+}
