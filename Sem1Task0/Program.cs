876b72d8614070a8d40af9ca2a26a51af8201b20
NordLorK <nordlork@rambler.ru>
2023-01-15 16:57:51 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №0
+// Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на
+// само себя).
+
+// Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+
+// Проверим, чтобы данные были не пустыми
+if (inputNum != null)
+{
+  // // Парсим введённое число
+  // int number = int.Parse(inputNum);
+  // // Находим квадрат числа
+  // // int result = number * number;
+  // int result = (int)Math.Pow(number, 2);
+  // // Выводим данные в консоль
+  // Console.WriteLine(result);
+  Console.WriteLine(Math.Pow(int.Parse(inputNum), 2));
+}
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число
+// квадратом второго.
+
+// Считываем данные с консоли 1 число
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли 2 число
+string? inputNum2 = Console.ReadLine();
+
+// Проверим, чтобы данные были не пустыми
+if (inputNum1 != null && inputNum2 != null)
+{
+  // Парсим введённое 1 число
+  int number1 = int.Parse(inputNum1);
+  // Парсим введённое 2 число
+  int number2 = int.Parse(inputNum2);
+
+  if (number1 == number2 * number2)
+  {
+    Console.WriteLine("Первое число квадрат второго!");
+  }
+  else
+  {
+    Console.WriteLine("Первое число не квадрат второго!");
+  }
+}
