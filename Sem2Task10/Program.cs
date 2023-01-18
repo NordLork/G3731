1690058f057014528cd915019c48d23ff122c1c4
NordLorK <nordlork@rambler.ru>
2023-01-18 20:13:55 +0300

    add solution for task 10,13,15

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// Вариант 1
+// Console.Write("Введите трёхзначное число: ");
+// int num = int.Parse(Console.ReadLine() ?? "0");
+// if ((num > 99) && (num < 1000))
+// {
+//   int res = num / 10;
+//   res = res % 10;
+//   Console.WriteLine("Вторая цифра трёхзначного числа: " + res);
+// }
+// else
+// {
+//   Console.WriteLine("Число не трёхзначное.");
+// }
+
+// Вариант 2
+Console.Write("Введите трёхзначное число: ");
+char[] digits = Console.ReadLine().ToCharArray();
+// Проверка ввода
+if (digits.Length == 3)
+{
+  // Выводим цифру 
+  Console.WriteLine("Вторая цифра трёхзначного числа: " + digits[1]);
+}
+else
+{
+  Console.WriteLine("Число не трёхзначное.");
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру 
+// заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// Вариант 1
+// Console.Write("Введите число: ");
+// char[] digits = Console.ReadLine().ToCharArray();
+// if (digits.Length > 3)
+// {
+//   Console.WriteLine("Третья цифра: " + digits[2]);
+// }
+// else
+// {
+//   Console.WriteLine("Третьей цифры нет.");
+// }
+
+// Вариант 2
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+// Счётчик разряда числа
+int i = 0;
+int raz = num;
+while (raz > 0)
+{
+  raz /= 10;
+  i++;
+}
+// Проверка и вывод результата
+if (i > 2)
+{
+  num = num / (int)Math.Pow(10, i - 3);
+  num = num % 10;
+  Console.WriteLine("Третья цифра: " + num);
+}
+else
+{
+  Console.WriteLine("Третьей цифры нет.");
+}
