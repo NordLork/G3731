34a27d9daed43d1198bde6d1ae8e037e2688a893
NordLorK <nordlork@rambler.ru>
2023-01-28 17:51:24 +0300

    add update solution for task 0,30

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -9,12 +9,27 @@ string? inputNum = Console.ReadLine();
 // Проверим, чтобы данные были не пустыми
 if (inputNum != null)
 {
-  // // Парсим введённое число
-  // int number = int.Parse(inputNum);
+  // Парсим введённое число
+  int number = int.Parse(inputNum);
   // // Находим квадрат числа
   // // int result = number * number;
   // int result = (int)Math.Pow(number, 2);
   // // Выводим данные в консоль
   // Console.WriteLine(result);
-  Console.WriteLine(Math.Pow(int.Parse(inputNum), 2));
+
+  //Console.WriteLine(Math.Pow(int.Parse(inputNum), 2));
+
+  int result = number * number;
+  //===========================================================
+  // Оценка скорости
+  //===========================================================
+  DateTime d1 = DateTime.Now;
+  for (int i = 0; i < 10000000; i++)
+  {
+    result = number * number;
+  }
+  DateTime d2 = DateTime.Now;
+  Console.WriteLine(d2 - d1);
+  //===========================================================
+
 }
