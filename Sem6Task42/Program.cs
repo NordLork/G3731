82009f745fffebb495bf6e8d6733398f29457e2f
NordLorK <nordlork@rambler.ru>
2023-01-31 23:37:34 +0300

    add solution for task 39,40,42,44,45

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача №42
+// Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вывод значения
+void PrintData(string res)
+{
+  Console.WriteLine(res);
+}
+
+// Метод перевода в двоичное число
+string toBinaryNumberSystem(int a)
+{
+  string numBinary = "0";
+  if (a == 0) return numBinary;
+  else
+  {
+    while (a > 0)
+    {
+      numBinary = Convert.ToString(a % 2) + numBinary;
+      a = a / 2;
+    }
+  }
+  return numBinary;
+}
+
+int number = -1;
+while (number < 0) number = ReadData("Введите целое число: ");
+string res = toBinaryNumberSystem(number);
+PrintData("Введённое число в двоичном виде: " + res);
---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача №45
+// Напишите программу, которая будет создавать 
+// копию заданного одномерного массива с 
+// помощью поэлементного копирования.
+
+// Запрос значения
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генерируем массив
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+  int[] arr = new int[len];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return arr;
+}
+
+// Вывод массива
+void Print1DArr(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Поэлементное копирование заданного массива
+int[] copyArray(int[] arr)
+{
+  int[] copy = new int[arr.Length];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    copy[i] = arr[i];
+  }
+  return copy;
+}
+
+int len = ReadData("Введите длинну массива: ");
+int num1 = ReadData("Введите минимум для значений в массиве: ");
+int num2 = ReadData("Введите максимум для значений в массиве: ");
+int[] testArr = Gen1DArray(len, num1, num2);
+Print1DArr(testArr);
+Console.WriteLine();
+int[] copyArr = copyArray(testArr);
+Console.WriteLine();
+Print1DArr(copyArr);
