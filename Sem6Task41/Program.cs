fc246c534783e4025d191b85592be1483a0f014b
NordLorK <nordlork@rambler.ru>
2023-02-01 10:17:18 +0300

    add solution for task 41,43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// * Пользователь вводит число нажатий, затем программа 
+// следит за нажатиями с клавиатуры и выдает сколько 
+// чисел больше 0 было введено.
+
+// Запрос ручного ввода массива
+int[] ReadArray(string msg)
+{
+  string str = string.Empty;
+  while ((str == string.Empty) || (str == " "))
+  {
+    Console.Write(msg);
+    str = Console.ReadLine() ?? "0";
+  }
+  int[] arr = str.Split(" ".ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+  return arr;
+}
+
+// Вывод массива
+void Print1DArr(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Считаем элементы больше 0
+int CountElem(int[] arr)
+{
+  int res = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] > 0)
+    {
+      res++;
+    }
+  }
+  return res;
+}
+
+// Метод печати результата пользователя
+void PrintData(string msg)
+{
+  Console.WriteLine(msg);
+}
+
+int[] array = ReadArray("Введите элементы массива (через пробел): ");
+// Console.WriteLine();
+Print1DArr(array);
+int count = CountElem(array);
+PrintData($"Количество элементов массива больше 0 равно {count}.");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт 
+// точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Запрос ввода
+double ReadData(string msg)
+{
+  Console.Write(msg);
+  return Convert.ToDouble(Console.ReadLine() ?? "0");
+}
+
+// Определение пересечения прямых
+(double x, double y) getXY(double k1, double b1, double k2, double b2)
+{
+  double getX = (b2 - b1) / (k1 - k2);
+  double getY = k1 * getX + b1;
+  getX = Math.Round(getX, 3);
+  getY = Math.Round(getY, 3);
+  var result = (x: getX, y: getY);
+  return result;
+}
+
+// Метод печати результата пользователя
+void PrintData(string msg)
+{
+  Console.WriteLine(msg);
+}
+
+double k1 = ReadData("Введите k1: ");
+double b1 = ReadData("Введите b1: ");
+double k2 = ReadData("Введите k2: ");
+double b2 = ReadData("Введите b2: ");
+var coord = getXY(k1, b1, k2, b2);
+PrintData($"Пересечение в точке: ({coord.x}; {coord.y})");
