2dc4be0459791495f449d5f184febbc2d830e454
NordLorK <nordlork@rambler.ru>
2023-02-04 18:26:58 +0300

    add solution for task 39,40,42,43,44,45 update

--- Sem6Task43/Program.cs
+++ Sem6Task43/Program.cs
@@ -34,3 +34,41 @@ double k2 = ReadData("Введите k2: ");
 double b2 = ReadData("Введите b2: ");
 var coord = getXY(k1, b1, k2, b2);
 PrintData($"Пересечение в точке: ({coord.x}; {coord.y})");
+
+
+
+// Вариант 2
+
+// int ReadData(string line)
+// {
+//     Console.Write(line);
+//     int number = int.Parse(Console.ReadLine() ?? "0");
+//     return number;
+// }
+// // печать результата
+// void PrintData (string prefix, string data) //разбили на две части вывод в консоль
+// {
+//      Console.WriteLine(prefix + data); //сначала выводим prefix + а потом данные data\
+// }
+// // Поиск точки пересечения двух прямых
+// double[] PointFind (int k1, int b1, int k2, int b2)
+// {
+//     double[] outArr = new double[2];
+//     double x = (double)(b2 - b1) / (double)(k1 - k2);
+//     double y = k1 * x + b1;
+//     outArr[0] = x;
+//     outArr[1] = y;
+//     return outArr;
+// }
+// //Ввод значения k1, b1, k2, b2
+// int k1 =ReadData("Введите значение k1:");
+// int b1 =ReadData("Введите значение b1:");
+// int k2 =ReadData("Введите значение k2:");
+// int b2 =ReadData("Введите значение b2:");
+
+// //Вычисляем точку пересечения
+// double[]Point = PointFind (k1, b1, k2, b2);
+// //Выводим на экран координату точки пересечения
+// PrintData ("Точка пересечения двух прямых: ", $"({Point[0]};{Point[1]})");
+
+// // но почему то не получается то что надо..
--- Sem6Task44/Program.cs
+++ Sem6Task44/Program.cs
@@ -36,3 +36,40 @@ void Print1DArray(int[] arr)
 
 int num = ReadData("Введите число: ");
 Print1DArray(Fibonacci(num));
+
+
+
+// Вариант 2
+
+// int number = ReadData("Введите число:");
+// PrintData(FibNum(number));
+
+// //Метод, считывающий данные, введенные пользователем
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+// //Метод, выводящий на печать данные пользователю
+// void PrintData(string res)
+// {
+//     Console.WriteLine(res);
+// }
+
+// //Числа Фибоначчи
+// string FibNum(int num)
+// {
+//     string outLine = "0 1";
+//     int first = 0;
+//     int last = 1;
+//     int buf = 0;
+//     for (int i = 2; i < num; i++)
+//     {
+//         outLine = outLine +" "+ (first + last).ToString();
+//         buf=last;
+//         last=first+last;
+//         first=buf;
+//     }
+//     return outLine;
+// }
