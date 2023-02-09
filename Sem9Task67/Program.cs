487db755332c40784a851e19983c73248611347e
NordLorK <nordlork@rambler.ru>
2023-02-10 00:44:05 +0300

    add solution for task 63,65,67,69

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 67
+// примает на вход число и возвращает сумму числа
+// ---------------------------------------------------------------------
+
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод, принимает число, выводит в консоль
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Метод, принимает строку, выводит в консоль
+static void PrintResultStr(string line)
+{
+    Console.WriteLine(line);
+}
+
+//Сумма цифр в числе рекурентно
+int RecSumDig(int n)
+{
+    if(n<=10)return n;
+    return n%10+RecSumDig(n/10);
+}
+
+//Сумма цифр в числе 
+int SumDig(int n)
+{
+    int res = 0;
+    while(n>0)
+    {
+        res=res+n%10;
+        n=n/10;
+    }
+    return res;
+}
+
+
+int numberN = ReadData("Enter number N: ");
+DateTime d1 = DateTime.Now;
+int Sum1 = RecSumDig(numberN);
+PrintResult(Sum1);
+PrintResultStr("Решение суммы цифр в числе рекурентно: " + (DateTime.Now - d1));
+//Console.WriteLine();
+DateTime d2 = DateTime.Now;
+int Sum2 = SumDig(numberN);
+PrintResult(Sum2);
+PrintResultStr("Решение суммы цифр циклом: " + (DateTime.Now - d2));
